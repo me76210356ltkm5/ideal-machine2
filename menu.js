c152522555b389a4ef1a845133c94d5cba92a453
me76210356ltkm5 <48250055+me76210356ltkm5@users.noreply.github.com>
2019-04-13 21:41:11 +0900

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,5 +2,6 @@ $(function(){
 var accessTag=$(".Access-tag");
 accessTag.click(function(){
 window.location.href="https://me76210356ltkm5.github.io/ideal-machine/";
+  $("html,body").animate({scrollTop:sixthWrapper},{duration:1000});
 })
 });
